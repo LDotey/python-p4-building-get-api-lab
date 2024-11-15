d4e0f22263bed5763e7d04d80d95dbd36c1342f7
Leigh Dotey <leighdotey@gmail.com>
2024-11-15 11:19:02 -0800

    complete lab

--- server/app.py
+++ server/app.py
@@ -2,6 +2,7 @@
 
 from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
+from sqlalchemy import desc
 
 from models import db, Bakery, BakedGood
 
@@ -18,21 +19,56 @@ db.init_app(app)
 def index():
     return '<h1>Bakery GET API</h1>'
 
+# GET /bakeries: returns a list of JSON objects 
+# for all bakeries in the database.
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries = [bakery.to_dict() for bakery in Bakery.query.all()]
 
+    response = make_response(bakeries, 200)
+    return response
+# GET /bakeries/<int:id>: returns a single bakery as JSON 
+# with its baked goods nested in a list. 
+# Use the id from the URL to look up the correct bakery
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter(Bakery.id == id).first()
+    bakery_dict = bakery.to_dict()
 
+    response = make_response(bakery_dict, 200)
+    return response
+
+# GET /baked_goods/by_price: returns a list of baked goods 
+# as JSON, sorted by price in descending order. 
+# (HINT: how can you use SQLAlchemy to sort the 
+# baked goods in descending order?)
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
 
+    baked_goods = BakedGood.query.order_by(desc(BakedGood.price)).all()
+
+    baked_goods_dict = [baked_good.to_dict() for baked_good in baked_goods]
+
+    response = make_response(baked_goods_dict, 200)
+
+    return response
+    
+
+# GET /baked_goods/most_expensive: returns the single 
+# most expensive baked good as JSON. 
+# (HINT: how can you use SQLAlchemy to sort the 
+# baked goods in descending order and 
+# limit the number of results?)
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+
+    baked_good = BakedGood.query.order_by(desc(BakedGood.price)).first()
+
+    baked_good_dict = baked_good.to_dict()
+
+    response = make_response(baked_good_dict, 200)
+
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
